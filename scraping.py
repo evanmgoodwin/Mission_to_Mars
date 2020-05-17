27b957b5c71cbbc1e3b91d54f3ba83209a2ab4b8
evanmgoodwin <62187264+evanmgoodwin@users.noreply.github.com>
2020-05-17 11:49:05 -0700

    uploading scraping file

--- scraping.py
+++ scraping.py
@@ -21,6 +21,10 @@ def scrape_all():
             "news_paragraph": news_paragraph,
             "featured_image": featured_image(browser),
             "facts": mars_facts(),
+            "hemi_1": hemi_1(browser),
+            "hemi_2": hemi_2(browser),
+            "hemi_3": hemi_3(browser),
+            "hemi_4": hemi_4(browser),
             "last_modified": dt.datetime.now()
     }
 
@@ -111,6 +115,104 @@ def mars_facts():
 
     # Convert dataframe into HTML format and add bootstrap
     return df.to_html()
+
+
+def hemi_1(browser):
+
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    browser.is_element_present_by_text("cerberus", wait_time=1)
+    hemi_1_elem = browser.find_link_by_partial_text("Cerberus Hemisphere Enhanced")
+    hemi_1_elem.click()
+
+    html = browser.html
+    img_soup = BeautifulSoup(html, "html.parser")
+
+    try:
+        
+        img_url_rel = img_soup.select_one("img.wide-image").get("src")
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    img_url = f"https://astrogeology.usgs.gov{img_url_rel}"
+
+    return img_url
+
+def hemi_2(browser):
+
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    browser.is_element_present_by_text("schiaparelli", wait_time=1)
+    hemi_1_elem = browser.find_link_by_partial_text("Schiaparelli Hemisphere Enhanced")
+    hemi_1_elem.click()
+
+    html = browser.html
+    img_soup = BeautifulSoup(html, "html.parser")
+
+    try:
+        
+        img_url_rel = img_soup.select_one("img.wide-image").get("src")
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    img_url = f"https://astrogeology.usgs.gov{img_url_rel}"
+
+    return img_url
+
+def hemi_3(browser):
+
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    browser.is_element_present_by_text("syrtis major", wait_time=1)
+    hemi_1_elem = browser.find_link_by_partial_text("Syrtis Major Hemisphere Enhanced")
+    hemi_1_elem.click()
+
+    html = browser.html
+    img_soup = BeautifulSoup(html, "html.parser")
+
+    try:
+        
+        img_url_rel = img_soup.select_one("img.wide-image").get("src")
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    img_url = f"https://astrogeology.usgs.gov{img_url_rel}"
+
+    return img_url
+
+def hemi_4(browser):
+
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    browser.is_element_present_by_text("valles marineris", wait_time=1)
+    hemi_1_elem = browser.find_link_by_partial_text("Valles Marineris Hemisphere Enhanced")
+    hemi_1_elem.click()
+
+    html = browser.html
+    img_soup = BeautifulSoup(html, "html.parser")
+
+    try:
+        
+        img_url_rel = img_soup.select_one("img.wide-image").get("src")
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    img_url = f"https://astrogeology.usgs.gov{img_url_rel}"
+
+    return img_url
+
 
 if __name__ == "__main__":
     # If running as script, print scraped data
